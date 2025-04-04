6263840f2175b95dc24f8881820f21c302eaf36a
giddens7196 <mgidden4@gmail.com>
2025-04-04 14:57:12 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
+// file: src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import ProfilePage from './pages/profile'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
