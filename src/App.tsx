5c8085b5307945d9aa89aabe9f166669fca0b240
giddens7196 <mgidden4@gmail.com>
2025-04-09 11:19:22 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+// file: src/App.tsx
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import ProfilePage from "./pages/profile";
+import FeedPage from "./pages/feed";
+import FriendsPage from "./pages/friends";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/feed" element={<FeedPage />} />
+        <Route path="/friends" element={<FriendsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🏠 Welcome to Travlr</h1>
+      <p>Use the navigation above to get started.</p>
+    </div>
+  );
+}
+
+export default App;
