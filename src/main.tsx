c2fa8605cdd9da32ded5843f6cf98aa4994c97e4
giddens7196 <mgidden4@gmail.com>
2025-04-09 16:03:01 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 // file: src/main.tsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import ProfilePage from './pages/profile'
-import FeedPage from './pages/feed' // ✅ new route
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/feed" element={<FeedPage />} /> {/* ✅ now connected */}
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
-)
+);
