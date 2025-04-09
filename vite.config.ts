52cc54cfbdbc6d7fb8209905d5fdf544e44afd20
giddens7196 <mgidden4@gmail.com>
2025-04-09 11:35:30 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-// file: vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
-  server: {
-    // Ensures React Router handles navigation on Vercel
-    historyApiFallback: true,
-  },
-})
-
-
+});
